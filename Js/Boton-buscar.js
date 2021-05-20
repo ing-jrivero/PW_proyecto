7c02d430179d53eb03658975ed769583730c99ea
ing_Jrivero <a17490345@itmexicali.edu.mx>
2021-05-19 19:02:02 -0700

    Archivo Js con la funcion de busqueda para el boton de busqueda principal.

---
+++ Js/Boton-buscar.js
@@ -0,0 +1,34 @@
+
+function buscar()
+{
+    
+    let busqueda=document.querySelectorAll(".buscar > .buscar_texto")[0].value
+    let texto=busqueda.toLowerCase();
+    let re_guitarras=/(guitarras?)/i;
+    let re_baterias=/(baterias?)/i;
+    let re_bosinas=/(bosinas?)/i;
+  //  alert(texto)
+    if(re_guitarras.test(texto))
+    {
+        alert("buscaste guitarras")
+        
+    }
+    else if(re_baterias.test(texto))
+    {
+        alert("buscaste baterias")
+
+    }
+    else if(re_bosinas.test(texto))
+    {
+
+    }
+    else
+    {
+        alert("")
+
+    }
+
+
+
+
+}
